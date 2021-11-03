bf51743307da261dc5fdd909d62aae380c8cd469
Jelena Prčetić <jelenap@comtrade.com>
2021-11-03 15:30:09 +0100

    FlatList

--- screens/ProductsScreen.js
+++ screens/ProductsScreen.js
@@ -1,20 +1,16 @@
 import React from "react";
-import { View, Text, FlatList } from "react-native";
+import { Text, FlatList } from "react-native";
 import { useSelector } from "react-redux";
 
 import { PRODUCTS } from "../data/dummy-data";
 
+
 const ProductsList = (props) => {
-  const renderProductsItem = (itemData) => {
-    return (
-      <View>
-        <Text>{title = itemData.item.title}</Text>
-      </View>
-    );
-  };
-  return (
-    <FlatList data={PRODUCTS} renderItem={renderProductsItem} numColumns={1} />
-  );
+  const products = useSelector((state) => state.products.products);
+  console.log(products);
+  const renderProductsItem = (itemData) => <Text>{itemData.item.title}</Text>
+  
+  return <FlatList data={PRODUCTS} renderItem={renderProductsItem} numColumns={1} />
 };
 
 export default ProductsList;
