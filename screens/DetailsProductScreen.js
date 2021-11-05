d8d4e77326b2bf03ce81ede383673db7b7c8e55b
Jelena Prčetić <jelenap@comtrade.com>
2021-11-05 12:11:56 +0100

    Style DetailsProductScreen

--- screens/DetailsProductScreen.js
+++ screens/DetailsProductScreen.js
@@ -1,31 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import CustomButton from '../components/CustomButton';
 
 import { PRODUCTS } from '../data/dummy-data';
 
 const DetailsProductScreen = (props) => {
   const productId = props.navigation.getParam('productId');
-  const  selectedProduct = PRODUCTS.find(product => product.id === productId);
+  const selectedProduct = PRODUCTS.find((product) => product.id === productId);
   return (
-    <View style={styles.screen}>
-      <Text>{selectedProduct.title}</Text>
-    </View>
+    <ScrollView>
+      <Image source={{ uri: selectedProduct.imageUrl }} style={styles.image} />
+      <View style={styles.buttonStyles}>
+        <CustomButton title="ADD TO CART" />
+      </View>
+      <Text style={styles.priceStyles}>${selectedProduct.price}</Text>
+      <Text style={styles.descriptionStyles}>{selectedProduct.description}</Text>
+    </ScrollView>
   );
 };
 
-DetailsProductScreen.navigationOptions = navigationData => {
+DetailsProductScreen.navigationOptions = (navigationData) => {
   const productId = navigationData.navigation.getParam('productId');
-  const  selectedProduct = PRODUCTS.find(product => product.id === productId);
+  const selectedProduct = PRODUCTS.find((product) => product.id === productId);
   return {
-    headerTitle: selectedProduct.title
+    headerTitle: selectedProduct.title,
   };
-}; 
+};
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  buttonStyles: {
+    flexDirection: 'row',
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  priceStyles: {
+    fontSize: 30,
+    fontFamily: 'open-sans',
+    textAlign: 'center',
+    paddingTop: 20,
+  },
+  descriptionStyles: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 18,
+    textAlign: 'center',
+    paddingTop: 50,
   },
 });
 
