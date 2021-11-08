3b78eb07def5ee98f53d63aa4aee90b9cc5702c8
Marija Kulić <72737390+marijakulic@users.noreply.github.com>
2021-11-08 14:53:56 +0100

    Revert "Cart Icon (#18)" (#20)
    This reverts commit 6aac8ed024d949ac3df1cf6b46069f242e6b9a67.

--- screens/ProductsScreen.js
+++ screens/ProductsScreen.js
@@ -2,12 +2,9 @@ import React from 'react';
 import { FlatList } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import CardWrapper from '../components/CardWrapper';
 import { addProduct } from '../store/action/cart';
-import DetailsProductScreen from './DetailsProductScreen';
-import HeaderButton from "../components/HeaderButton";
 
 const ProductsList = (props) => {
   const products = useSelector((state) => state.products.products);
@@ -36,14 +33,5 @@ const ProductsList = (props) => {
 
   return <FlatList data={products} renderItem={renderProductsItem} numColumns={1} />;
 };
-
-ProductsList.navigationOptions = (navData) => {
-  return {
-    headerTitle: "Products",
-    headerRight:() => <HeaderButtons HeaderButtonComponent={HeaderButton}>
-      <Item title="Cart" iconName="md-cart" onPress={()=>{navData.navigation.navigate({routeName: 'Cart'})}} />
-    </HeaderButtons>
-  };
-}; 
 
 export default ProductsList;
