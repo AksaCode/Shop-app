3c12172726efb37e933314eb4f8bdb70690ae1cb
marija_kulic <99marijakulic@gmail.com>
2021-11-09 16:03:00 +0100

    OrderItem screen and configuration

--- navigation/ProductsNavigator.js
+++ navigation/ProductsNavigator.js
@@ -1,13 +1,14 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-import DetailsProductsScreen from '../screens/DetailsProductScreen';
 import ProductsScreen from '../screens/ProductsScreen';
 import CartScreen from '../screens/CartScreen';
+import DetailsProductScreen from '../screens/DetailsProductScreen';
+import OrderScreen from '../screens/OrderScreen';
 
 const ProductsNavigator = createStackNavigator({
   Products: ProductsScreen,
-  Details: DetailsProductsScreen,
+  Details: OrderScreen,
   Cart: CartScreen,
 });
 
--- store/reducer/orderReducer.js
+++ store/reducer/orderReducer.js
@@ -7,8 +7,8 @@ const orderReducer = (state = initState, action) => {
   switch (action.type) {
     case CREATOR:
       if (action.type === CREATOR) {
-        const order = new Order(Math.random(), action.cartItems, action.totalAmount, new Date());
-        return { orders: [...state.orders, order, action.totalAmount] };
+        const order = new Order(Math.random(), action.cartItems, action.totalAmount, new Date().toString());
+        return { orders: [...state.orders, order] };
       }
   }
   return state;
