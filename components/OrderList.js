3892d853baeb5f899b2c8f021e2fa9d13c7d4beb
Dusan Aksic <aksacode@gmail.com>
2021-11-10 13:53:42 +0100

    deletting from cart

--- components/OrderList.js
+++ components/OrderList.js
@@ -1,15 +1,20 @@
-import React from "react";
-import { FlatList } from "react-native";
-import { useSelector } from "react-redux";
-import moment from "moment";
+import React from 'react';
+import { FlatList } from 'react-native';
+import { useSelector } from 'react-redux';
+import moment from 'moment';
 
-import OrderItem from "./OrderItem";
+import OrderItem from './OrderItem';
 
 const OrderList = (props) => {
-    const orders = useSelector((state) => state.orders.orders);
-    const renderOrder = (itemData) => <OrderItem productPrice={itemData.item.totalAmount} date={moment(itemData.item.date).format("MMMM Do YYYY, h:mm")} />
+  const orders = useSelector((state) => state.orders.orders);
+  const renderOrder = (itemData) => (
+    <OrderItem
+      productPrice={itemData.item.totalAmount}
+      date={moment(itemData.item.date).format('MMMM Do YYYY, h:mm')}
+    />
+  );
 
-    return <FlatList data={orders} renderItem={renderOrder} numColumn={1} />;
+  return <FlatList data={orders} renderItem={renderOrder} numColumn={1} />;
 };
 
 export default OrderList;
--- store/action/cart.js
+++ store/action/cart.js
@@ -1,10 +1,15 @@
 export const ADD_PRODUCT = 'ADD_PRODUCT';
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
+export const DELETE_LIST = 'DELETE_LIST';
 
 export const addProduct = (product) => {
   return { type: ADD_PRODUCT, product: product };
 };
 
 export const deleteProduct = (id) => {
-  return { type: DELETE_PRODUCT, id: id};
+  return { type: DELETE_PRODUCT, id: id };
+};
+
+export const deleteList = (items) => {
+  return { type: DELETE_LIST, items: items };
 };
