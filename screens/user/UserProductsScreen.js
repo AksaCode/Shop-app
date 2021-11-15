8e1ab3db020790df3a189aaef0a44fd01394c305
AksaCode <88269315+AksaCode@users.noreply.github.com>
2021-11-15 10:33:51 +0100

    adding user products screen (#30)
    * user product screen only
    
    * added card wrapper

---
+++ screens/user/UserProductsScreen.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet, FlatList } from 'react-native';
+import { useSelector } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import CardWrapper from '../../components/CardWrapper';
+import HeaderButton from '../../components/HeaderButton';
+import RowButtons from '../../components/RowButtons';
+
+const renderUserProductsItem = (itemData) => (
+  <CardWrapper
+    image={itemData.item.imageUrl}
+    title={itemData.item.title}
+    price={itemData.item.price}
+    cardAction={() => {}}
+  >
+    <RowButtons rightAction={() => {}} leftAction={() => {}} leftTitle="edit" rightTitle="delete" />
+  </CardWrapper>
+);
+
+const UserProductsScreen = (props) => {
+  const userProducts = useSelector((state) => state.products.userProducts);
+  return <FlatList data={userProducts} renderItem={renderUserProductsItem} numColumns={1} />;
+};
+
+UserProductsScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: 'Your products',
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
+};
+
+export default UserProductsScreen;
+
+const styles = StyleSheet.create({});
