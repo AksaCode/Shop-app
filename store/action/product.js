4df24a14a1460561387f48593ee3d4156cfedae5
Jelena Prčetić <jelenap@comtrade.com>
2021-11-17 11:41:01 +0100

    kom

--- store/action/product.js
+++ store/action/product.js
@@ -1,5 +1,10 @@
 export const DELETE = 'DELETE';
+export const ADD = 'ADD';
 
 export const deleteOnClick = (id) => {
   return { type: DELETE, id: id };
+};
+
+export const addP = (title, description, imageUrl, price) => {
+  return { type: ADD, product: { title: title, description: description, imageUr: imageUrl, price: price } };
 };
--- store/reducer/products.js
+++ store/reducer/products.js
@@ -1,17 +1,30 @@
 import { PRODUCTS } from '../../data/dummy-data';
 import { DELETE } from '../action/product';
+import { ADD } from '../action/product';
+
+import Product from '../../model/product';
 
 const initialState = {
   products: PRODUCTS,
   userProducts: PRODUCTS.filter((item) => item.ownerId === 'u1'),
+  //dodajem preko rest operatora novi product
 };
 
 const productsReducer = (state = initialState, action) => {
   switch (action.type) {
     case DELETE:
+      if(action.type===DELETE){
       let newList = [];
       newList = state.userProducts.filter((item) => item.id !== action.id);
       return { ...state, userProducts: [...newList] };
+      }
+    case ADD:
+      if(action.type===ADD)
+      {
+        const exIndex = state.userProducts.findIndex((item) => item.id === action.id);
+        let product=new Product(Math.random(),'u1', action.product.title, action.product.imageUrl, action.product.description, action.product.price);
+        console.log(product);
+    }
     default:
       return state;
   }
