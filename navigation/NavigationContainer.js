9d04801ec9af905419a8986e08986ce7e8fd3914
Dusan Aksic <aksacode@gmail.com>
2021-12-21 18:22:38 +0100

    startup bug fixed

--- navigation/NavigationContainer.js
+++ navigation/NavigationContainer.js
@@ -8,9 +8,11 @@ const NavigationContainer = (props) => {
   const navRef = useRef();
   const isAuth = useSelector((state) => !!state.auth.token);
   useEffect(() => {
-    if (!isAuth) {
-      navRef.current.dispatch(NavigationActions.navigate({ routeName: 'Auth' }));
-    }
+    setTimeout(() => {
+      if (!isAuth) {
+        navRef.current.dispatch(NavigationActions.navigate({ routeName: 'Auth' }));
+      }
+    }, 3000);
   }, [isAuth]);
   return <ProductsNavigator ref={navRef} />;
 };
