d3d8c72f1c022bf0aeb064a7f427393fc3c46195
Dusan Aksic <aksacode@gmail.com>
2021-12-24 10:42:33 +0100

    input and inputPass changed

--- components/InputPass.js
+++ components/InputPass.js
@@ -2,58 +2,7 @@ import React, { useReducer, useEffect, useState } from 'react';
 import { View, Text, TextInput, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-const INPUT_CHANGE = 'INPUT_CHANGE';
-const INPUT_BLUR = 'INPUT_BLUR';
-
-const inputReducer = (state, action) => {
-  switch (action.type) {
-    case INPUT_CHANGE:
-      return {
-        ...state,
-        value: action.value,
-        isValid: action.isValid,
-      };
-    case INPUT_BLUR:
-      return {
-        ...state,
-        touched: true,
-      };
-    default:
-      return state;
-  }
-};
-
 const InputPass = (props) => {
-  const [icon, setIcon] = useState('ios-eye-off');
-  const [hidePassword, setHidePassword] = useState(true);
-  const [inputState, dispatch] = useReducer(inputReducer, {
-    value: props.initialValue ? props.initialValue : '',
-    isValid: props.initiallyValid,
-    touched: false,
-  });
-
-  const { onInputChange, id } = props;
-
-  useEffect(() => {
-    if (inputState.touched) {
-      onInputChange(id, inputState.value, inputState.isValid);
-    }
-  }, [inputState, onInputChange, id]);
-
-  const textChangeHandler = (text) => {
-    const emailRegex =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    let isValid = true;
-    if (props.minLength != null && text.length > props.minLength) {
-      isValid = false;
-    }
-    dispatch({ type: INPUT_CHANGE, value: text, isValid: isValid });
-  };
-
-  const lostFocusHandler = () => {
-    dispatch({ type: INPUT_BLUR });
-  };
-
   _changeIcon = () => {
     icon !== 'ios-eye-off' ? (setIcon('ios-eye-off'), setHidePassword(false)) : (setIcon('eye'), setHidePassword(true));
   };
