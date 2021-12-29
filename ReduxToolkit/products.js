828bf37e4cc78359b25b06967a1a966662b3ce6b
AksaCode <88269315+AksaCode@users.noreply.github.com>
2021-12-29 13:20:07 +0100

    updated products thunk (#92)

--- ReduxToolkit/products.js
+++ ReduxToolkit/products.js
@@ -1,76 +1,114 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-export const getProducts = createAsyncThunk('products/getProducts', async (data, { getState, dispatch }) => {
-  const userId = getState().auth.userId;
-  const response = await fetch('https://shop-application-comtrade-default-rtdb.firebaseio.com//products.json');
-  const resData = await response.json();
-  let fromGetProducts = { items: { ...resData }, userId: userId };
-  return fromGetProducts;
-});
+export const getProducts = () => {
+  return async (dispatch, getState) => {
+    const userId = getState().auth.userId;
+    try {
+      const response = await fetch('https://shop-application-comtrade-default-rtdb.firebaseio.com/products.json');
+      if (!response.ok) {
+        throw new Error('Response is not 200');
+      }
+      const resData = await response.json();
+      const loadedProducts = [];
+      for (const key in resData) {
+        loadedProducts.push({
+          id: key,
+          ownerId: resData[key].ownerId,
+          title: resData[key].title,
+          imageUrl: resData[key].imageUrl,
+          description: resData[key].description,
+          price: resData[key].price,
+        });
+      }
+      const objProducts = {
+        products: loadedProducts,
+        userProducts: loadedProducts.filter((prod) => prod.ownerId === userId),
+      };
+      dispatch(setProductsDispatch(objProducts));
+    } catch (error) {
+      throw error;
+    }
+  };
+};
 
-export const deleteOnClick = createAsyncThunk('products/deleteOnClick', async (id, { getState }) => {
-  const token = getState().auth.token;
-  const response = await fetch(
-    `https://shop-application-comtrade-default-rtdb.firebaseio.com//products/${id}.json?auth=${token}`,
-    {
-      method: 'DELETE',
-    },
-  );
-  if (!response.ok) {
-    throw new Error('Response is not 200');
-  }
-  return id;
-});
-
-export const addNewProduct = createAsyncThunk('products/addNewProduct', async (data, { getState }) => {
-  const token = getState().auth.token;
-  const userId = getState().auth.userId;
-
-  const response = await fetch(
-    `https://shop-application-comtrade-default-rtdb.firebaseio.com//products.json?auth=${token}`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+export const deleteOnClick = (id) => {
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
+    const response = await fetch(
+      `https://shop-application-comtrade-default-rtdb.firebaseio.com/products/${id}.json?auth=${token}`,
+      {
+        method: 'DELETE',
       },
-      body: JSON.stringify({
-        title: data.title,
-        imageUrl: data.imageUrl,
-        description: data.description,
-        price: data.price,
-        ownerId: userId,
-      }),
-    },
-  );
-  const responseData = await response.json();
-  const id = responseData.name;
-  const prodData = { ...data, ownerId: userId, id };
-  return prodData;
-});
-export const editProduct = createAsyncThunk('products/editProduct', async (data, { getState }) => {
-  const token = getState().auth.token;
-  const response = await fetch(
-    `https://shop-application-comtrade-default-rtdb.firebaseio.com//products/${data[0]}.json?auth=${token}`,
-    {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        title: data[1].title,
-        imageUrl: data[1].imageUrl,
-        description: data[1].description,
-      }),
-    },
-  );
-  if (!response.ok) {
-    throw new Error('Response is not 200');
-  }
-  const resData = await response.json();
-  const editParams = [resData, data[0]];
+    );
+    if (!response.ok) {
+      throw new Error('Response is not 200');
+    }
+    dispatch(deleteProductDispatch(id));
+  };
+};
 
-  return editParams;
-});
+export const addNewProduct = (values) => {
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
+    const userId = getState().auth.userId;
+    const response = await fetch(
+      `https://shop-application-comtrade-default-rtdb.firebaseio.com/products.json?auth=${token}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: values.title,
+          imageUrl: values.imageUrl,
+          description: values.description,
+          price: values.price,
+          ownerId: userId,
+        }),
+      },
+    );
+    const responseData = await response.json();
+    const productData = {
+      id: responseData.name,
+      title: values.title,
+      imageUrl: values.imageUrl,
+      description: values.description,
+      price: values.price,
+      ownerId: userId,
+    };
+    dispatch(addNewProductDispatch(productData));
+  };
+};
+
+export const editProduct = (values) => {
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
+    const response = await fetch(
+      `https://shop-application-comtrade-default-rtdb.firebaseio.com//products/${values[0]}.json?auth=${token}`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: values[1].title,
+          imageUrl: values[1].imageUrl,
+          description: values[1].description,
+        }),
+      },
+    );
+    if (!response.ok) {
+      throw new Error('Response is not 200');
+    }
+    const editProductInfo = {
+      id: values[0],
+      title: values[1].title,
+      description: values[1].description,
+      imageUrl: values[1].imageUrl,
+    };
+    dispatch(editProductDispatch(editProductInfo));
+  };
+};
 
 export const productsSlice = createSlice({
   name: 'products',
@@ -78,31 +116,14 @@ export const productsSlice = createSlice({
     products: [],
     userProducts: [],
   },
-  extraReducers: {
-    [getProducts.pending]: (state) => {
-      state.products = [];
-      state.userProducts = [];
+  reducers: {
+    setProductsDispatch(state, action) {
+      state.products = action.payload.products.sort(function (a, b) {
+        return ('' + a.title).localeCompare(b.title);
+      });
+      state.userProducts = action.payload.userProducts;
     },
-    [getProducts.fulfilled]: (state, action) => {
-      const loadedProducts = [];
-      for (const key in action.payload.items) {
-        loadedProducts.push({
-          id: key,
-          ownerId: action.payload.items[key].ownerId,
-          title: action.payload.items[key].title,
-          imageUrl: action.payload.items[key].imageUrl,
-          description: action.payload.items[key].description,
-          price: action.payload.items[key].price,
-        });
-      }
-      state.products = [...loadedProducts];
-      state.userProducts = [...loadedProducts.filter((prod) => prod.ownerId === action.payload.userId)];
-    },
-    [getProducts.rejected]: (state) => {
-      state.products = [];
-      state.userProducts = [];
-    },
-    [deleteOnClick.fulfilled]: (state, action) => {
+    deleteProductDispatch(state, action) {
       let newList = [];
       newList = state.userProducts.filter((item) => item.id !== action.payload);
       let newListProducts = [];
@@ -110,52 +131,38 @@ export const productsSlice = createSlice({
       state.userProducts = [...newList];
       state.products = [...newListProducts];
     },
-    [addNewProduct.pending]: (state) => {
-      state.products = [...state.products];
-      state.userProducts = [...state.userProducts];
-    },
-    [addNewProduct.fulfilled]: (state, action) => {
+    addNewProductDispatch(state, action) {
       const product = {
-        id: action.id,
-        ownerId: action.ownerId,
-        title: action.title,
-        imageUrl: action.imageUrl,
-        description: action.description,
-        price: action.price,
+        id: action.payload.id,
+        ownerId: action.payload.ownerId,
+        title: action.payload.title,
+        imageUrl: action.payload.imageUrl,
+        description: action.payload.description,
+        price: action.payload.price,
       };
       state.products = state.products.concat(product);
       state.userProducts = state.userProducts.concat(product);
     },
-    [addNewProduct.rejected]: (state) => {
-      state.products = [...state.products];
-      state.userProducts = [...state.userProducts];
-    },
-    [editProduct.pending]: (state) => {
-      state.products = [...state.products];
-      state.userProducts = [...state.userProducts];
-    },
-    [editProduct.fulfilled]: (state, action) => {
-      const editIndex = state.userProducts.find((prod) => prod.id === action.payload[1]);
-      const product = {
-        id: action.payload[1],
-        ownerId: action.payload[0].ownerId,
-        title: action.payload[0].title,
-        imageUrl: action.payload[0].imageUrl,
-        description: action.payload[0].description,
+    editProductDispatch(state, action) {
+      const editIndex = state.userProducts.find((prod) => prod.id === action.payload.id);
+      const editedProduct = {
+        id: action.payload.id,
+        ownerId: action.payload.ownerId,
+        title: action.payload.title,
+        imageUrl: action.payload.imageUrl,
+        description: action.payload.description,
         price: editIndex.price,
       };
-      const editedMap = state.userProducts.filter((prod) => prod.id !== product.id);
-      const editedMapProducts = state.products.filter((prod) => prod.id !== product.id);
+      const editedMap = state.userProducts.filter((product) => product.id !== editedProduct.id);
+      const editedMapProducts = state.products.filter((product) => product.id !== editedProduct.id);
       state.products.splice(editIndex, 1);
-
-      state.products = [...editedMapProducts, product];
-      state.userProducts = [...editedMap, product];
-    },
-    [editProduct.rejected]: (state) => {
-      state.products = [...state.products];
-      state.userProducts = [...state.userProducts];
+      state.products = [...editedMapProducts, editedProduct];
+      state.userProducts = [...editedMap, editedProduct];
     },
   },
 });
+
+export const { setProductsDispatch, deleteProductDispatch, addNewProductDispatch, editProductDispatch } =
+  productsSlice.actions;
 
 export default productsSlice.reducer;
